496e0adf82856ea7bb57f9725f04d46b41baa680
awosborn <alanturing@example.coclear>
2021-01-02 14:28:43 -0700

    Add User story 1

---
+++ spec/features/machines/show_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe '', type: :feature do
+  describe 'As a visitor' do
+  
+
+    it 'I see a list of all snacks associated with a machine' do
+      dane = Owner.create!(name: 'Dane')
+      ozzys = Machine.create!(owner: dane, location: 'Danes')
+
+      kibble = Snack.create!(name:'Kih bul', price: 3.00)
+      bonz = Snack.create!(name:'bonz', price: 2.00)
+
+      MachineSnack.create!(snack_id: kibble.id, machine_id: ozzys.id)
+      MachineSnack.create!(snack_id: bonz.id, machine_id: ozzys.id)
+      
+      visit "/machines/#{ozzys.id}"
+      
+
+      expect(page).to have_content(kibble.name)
+      expect(page).to have_content(kibble.price)
+      expect(page).to have_content(bonz.name)
+      expect(page).to have_content(bonz.price)
+    end
+  end
+end
